6655cb5cdb3b9b6c484aa662284a0598b40399bf
Bastian Gruber <gruberbastian@me.com>
2019-05-22 09:49:12 +0200

    Add new git shortcuts

---
+++ fish/functions/gaa.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gaa --description 'alias gaa=git add .'
+	git add . $argv;
+end
---
+++ fish/functions/gc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gc --description 'alias gc=git commit'
+	git commit $argv;
+end
