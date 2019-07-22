fdcb086827cb58dc11cf55ea563a7f646e0e7ee6
Bastian Gruber <gruberbastian@me.com>
2019-07-22 14:04:33 -0400

    Add configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
 # Start X at login
 if status is-login
 	if test -z "$DISPLAY" -a $XDG_VTNR = 1 
-		exec startx -- -keeptty
+		exec ssh-agent startx -- -keeptty
 	end
 end
 
 set -x EDITOR nvim
+
+set PATH $HOME/.cargo/bin $PATH
